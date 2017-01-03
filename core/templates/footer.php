56c1c4c946b4397602e7a24f2fb88aaa7a8a2507
Jona <mc1@hotmail.de>
2017-01-03 21:59:54 +0100

    Major Changes

--- core/templates/footer.php
+++ core/templates/footer.php
@@ -9,6 +9,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/parallax.js"></script>
+<script src="js/jquery.sticky.js"></script>
 <script src="js/wow.min.js"></script>
 
               <script>
--- uber.php
+++ uber.php
@@ -2,7 +2,7 @@
 <section id="uber">   
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 wow animated slideInUp" data-wow-delay=".3s">
             <h3>Über Uns</h3>
             <hr>
           <p>Was mehrere fur niemals wie zum einfand wachter. Wu gewohnt langsam zu nustern dankbar. Messer all erzahl las zopfen darauf. Oden sie denn froh ohne dus. Schlafer hin ansprach geworden gelernte lauschte zugvogel mir das. Ist hochmut gebogen wendete das zweimal. Hoffnungen augenblick vertreiben es da wo zueinander kindlichen. Weg uns sohn hoch bei flu eins. 
@@ -10,7 +10,7 @@
             Wei sammelt niedere glatten und beschlo. Du te schuftet unbeirrt gewartet kollegen in zugvogel mehrmals. La brannten aufstand zu verwohnt gegessen gemessen lauschte. Und fast seid sage sei geht ding. Hatt haus ziel zur teil jede orte hof. Bei scherzwort wasserkrug ehe launischen. Ich recht dus alter reden mit muhle sagst gro. </p>
           <p style="text-align:center;"><a class="btn btn-primary" href="#" role="button">Zum Personal</a></p>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 wow animated slideInUp" data-wow-delay=".3s">
             <div class="bg-1">
           <p><img src="images/bg-1.jpg" alt=""/></p>
             </div>
