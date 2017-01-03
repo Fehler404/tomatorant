a879533048b59d88e0faa5e94a41ffc4231cfb77
Jona <mc1@hotmail.de>
2017-01-03 21:30:39 +0100

    Minor Changes

--- body.php
+++ body.php
@@ -13,7 +13,7 @@
     </div>
 </section>
 
-<nav class="navbar navbar-default navbar-static-top">
+   <nav class="navbar navbar-default navbar-static-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -22,21 +22,18 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"></a>
+          <a class="navbar-brand" href="#"><?php echo $website_name;?></a>
         </div>
-     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="/" class="smoothScroll"><i class="fa fa-home"></i></a></li>
-        <li><a class="smoothScroll" href="#aboutus">MENÜ</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ÜBER UNS <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="webdesign.php" class="smoothScroll">WEB-ENTWICKLUNG</a></li>
-            <li><a class="smoothScroll" href="https://edenco.de">HOSTING</a></li>
-            <li><a href="seo.php" class="smoothScroll">SEO</a></li>
-          </ul>
-        </li>
-        </ul>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
--- config.php
+++ config.php
@@ -4,3 +4,4 @@
     $website_title       = "Hallo Welt";
     $website_charset     = "UTF-8";
     $website_description = "Here website description";
+    $website_name        = "Tomatorant";
