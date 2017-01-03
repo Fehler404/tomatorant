ca61944ec56f5d4153f63638002e2ec1f70a4ec9
Jona <mc1@hotmail.de>
2017-01-03 21:41:29 +0100

    Major Changes

--- core/includes/links.php
+++ core/includes/links.php
@@ -1,10 +1,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/animate.css">
     <link href="css/main.css" rel="stylesheet">
-    <link rel="stylesheet" href="dist/css/bootstrap.offcanvas.min.css"/>
 	<link rel="stylesheet" href="css/animate.min.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
 	<script src='https://www.google.com/recaptcha/api.js'></script>
--- core/templates/footer.php
+++ core/templates/footer.php
@@ -5,14 +5,12 @@
         <p>&copy; 2016 Company, Inc.</p>
       </footer>
     </div> <!-- /container -->
-
-<script src="js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script src="js/parallax.js"></script>
 <script src="js/wow.min.js"></script>
-<script src="dist/js/bootstrap.offcanvas.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/core.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
               <script>
               new WOW().init();
               </script>
